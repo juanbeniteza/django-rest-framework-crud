e65f227a1ecdfb8e6168048775a272185fae0ce9
Juan Benitez <juanbenitez2908@gmail.com>
2018-06-29 18:52:17 -0500

    Model Created

---
+++ movies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
