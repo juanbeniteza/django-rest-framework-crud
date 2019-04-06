6c9d8cfc451e94db764c3321802df388782794cf
Juan Benitez <juan@fleks.info>
2019-04-06 14:29:28 -0500

    change url to path on routes urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
+from django.urls import include, path, re_path
 from . import views
 
 
 urlpatterns = [
-    url(r'^api/v1/movies/(?P<pk>[0-9]+)$', # urls with details i.e /movies/(1-9)
+    re_path(r'^api/v1/movies/(?P<pk>[0-9]+)$', # Url to get update or delete a movie
         views.get_delete_update_movie.as_view(),
         name='get_delete_update_movie'
     ),
-    url(
-        r'^api/v1/movies/$', # urls list all and create new one
+    path('api/v1/movies/', # urls list all and create new one
         views.get_post_movies.as_view(),
         name='get_post_movies'
     )
