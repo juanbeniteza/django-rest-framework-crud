180f36e3673330b69337b17d8e9228f0c8c04a16
EnterGodMode <15523163@students.uii.ac.id>
2019-04-06 17:24:20 +0700

    small changes for authentication

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -30,7 +30,11 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+   ),
 }
 
 
