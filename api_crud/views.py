12338e676441ace7facce3bccd193c08b905e29a
Juan Benitez <juan@fleks.info>
2019-04-11 09:35:06 -0500

    custom response on login view, now user info is returned

--- api_crud/views.py
+++ api_crud/views.py
@@ -6,7 +6,9 @@ from rest_framework import status
 from allauth.account.utils import complete_signup
 from allauth.account import app_settings as allauth_settings
 from rest_auth.models import TokenModel
+from rest_auth.views import LoginView
 from rest_auth.registration.app_settings import RegisterSerializer, register_permission_classes
+from django.contrib.auth.models import User
 
 sensitive_post_parameters_m = method_decorator(
     sensitive_post_parameters('password1', 'password2')
@@ -23,6 +25,7 @@ class RegisterView(CreateAPIView):
         return super(RegisterView, self).dispatch(*args, **kwargs)
 
     def create(self, request, *args, **kwargs):
+        print('here')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = self.perform_create(serializer)
@@ -39,3 +42,14 @@ class RegisterView(CreateAPIView):
 
         complete_signup(self.request._request, user, None, None)
         return user
+
+
+class CustomLoginView(LoginView):
+    def get_response(self):
+        orginal_response = super().get_response()
+        custom_response = {"user": {
+            "username": self.user.username,
+            "email": self.user.email
+        }}
+        orginal_response.data.update(custom_response)
+        return orginal_response
