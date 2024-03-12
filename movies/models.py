8f3d42a3085a1b506f448ee0e79e15890c0a1062
roran williams <rorannwilliams@gmail.com>
2024-03-12 11:33:29 +0300

    add option to change token expiration time

--- movies/models.py
+++ movies/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100)
-    year = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    creator = models.ForeignKey('auth.User', related_name='movies', on_delete=models.CASCADE)
+	id = models.BigAutoField(primary_key=True)
+	title = models.CharField(max_length=100)
+	genre = models.CharField(max_length=100)
+	year = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	creator = models.ForeignKey('auth.User', related_name='movies', on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ['-id']
-
-
+	class Meta:
+		ordering = ['-id']
